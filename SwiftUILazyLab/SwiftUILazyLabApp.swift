ea6f5d11ab576532bf9d0585e0180ad1843a212e
PortableDoughnut <gwen.thelin@gmail.com>
2025-01-30 09:33:45 -0700

    Initial Commit

---
+++ SwiftUILazyLab/SwiftUILazyLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILazyLabApp.swift
+//  SwiftUILazyLab
+//
+//  Created by Gwen Thelin on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILazyLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
